9b0286316a007e29ec5a609db4aab032ed045f90
monsor3s <64220885+monsor3s@users.noreply.github.com>
2023-05-01 17:20:35 -0300

    create text-header

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ediete o <code>src/App.js</code> e sabe para reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendendo React
-        </a>
-      </header>
+      <p className="text-header">SOBRE</p>
     </div>
   );
 }
