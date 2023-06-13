ce40a45c8b17487e7edc447429ae126f88edb68b
monsor3s <64220885+monsor3s@users.noreply.github.com>
2023-06-13 13:25:51 -0300

    criado o comportamento do checkbox selecionado

--- src/objects/Checkbox/index.js
+++ src/objects/Checkbox/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import "./styles.css"
+
 import Label from '../Label';
 import Input from '../Input';
 
--- src/objects/Input/index.js
+++ src/objects/Input/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Input = ({ id = "", value = "", type = "text" }) => {
   return (
-    <input id={id} type={type} value={value} />
+    <input id={id} type={type} value={value} className="input" />
   )
 }
 
