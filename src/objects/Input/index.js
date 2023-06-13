8812cbac2f234edefe6f18c6b31c9af6ecadfd9f
monsor3s <64220885+monsor3s@users.noreply.github.com>
2023-06-12 21:20:19 -0300

    deixar o type do input dinamico

--- src/objects/Input/index.js
+++ src/objects/Input/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import "./styles.css"
 import Label from '../Label';
 
-const Input = ({ id, value, content }) => {
+const Input = ({ id = "", value = "", content = "", type = "text" }) => {
   return (
     <>
-    <input id={id} type="checkbox" value={value} />
+    <input id={id} type={type} value={value} />
     <Label htmlFor={id} content={content} />
     </>
   )
